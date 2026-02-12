7ed5ca1d9c2eea1656aca7fe0a9130e4095c838a
Kubantov Muhamedzhan <mukhamedzhan.kubantov@narxoz.kz>
2026-02-12 19:18:44 +0500

    Complete Main demo output

--- src/main/java/com/narxoz/rpg/Main.java
+++ src/main/java/com/narxoz/rpg/Main.java
@@ -1,5 +1,11 @@
 package com.narxoz.rpg;
 
+import com.narxoz.rpg.character.Character;
+import com.narxoz.rpg.equipment.Armor;
+import com.narxoz.rpg.equipment.Weapon;
+import com.narxoz.rpg.equipment.WizardStaff;
+import com.narxoz.rpg.factory.*;
+
 /**
  * Main demonstration class for the RPG Character & Equipment System.
  *
@@ -18,6 +24,20 @@ package com.narxoz.rpg;
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== RPG Character & Equipment System ===\n");
+
+        // Factory Method: character creation via factories (no if-else for new Warrior/Mage/Archer)
+        CharacterFactoryRegistry characterRegistry = new CharacterFactoryRegistry();
+
+        Character warrior = characterRegistry.getFactory("warrior").createCharacter("Thor");
+        Character mage = characterRegistry.getFactory("mage").createCharacter("Gandalf");
+        Character archer = characterRegistry.getFactory("archer").createCharacter("Legolas");
+
+        // Abstract Factory: themed equipment families
+        demoCharacterWithEquipment(warrior, new MedievalEquipmentFactory());
+        System.out.println("\n---\n");
+        demoCharacterWithEquipment(mage, new MagicEquipmentFactory());
+        System.out.println("\n---\n");
+        demoCharacterWithEquipment(archer, new RangerEquipmentFactory());
 
         // TODO: Demonstrate Factory Method Pattern
         // Create different character types (Warrior, Mage, Archer, etc.)
@@ -55,6 +75,18 @@ public class Main {
 
 
         System.out.println("\n=== Demo Complete ===");
+    }
+    private static void demoCharacterWithEquipment(Character character, EquipmentFactory equipmentFactory){
+        character.displayStats();
+        character.useSpecialAbility();
+
+        Weapon weapon =equipmentFactory.createWeapon();
+        Armor armor =equipmentFactory.createArmor();
+
+        character.equipWeapon(weapon);
+        character.equipArmor(armor);
+
+        character.displayEquipment();
     }
 
     // TODO: Add helper methods as needed
